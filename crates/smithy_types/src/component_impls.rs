a55f6d11c40bbea3aff0856c961087cc52081fff
Robert Balicki <robert.balicki@gmail.com>
2019-01-06 17:02:54 -0500

    fix impl for Vector

--- crates/smithy_types/src/component_impls.rs
+++ crates/smithy_types/src/component_impls.rs
@@ -1,6 +1,9 @@
 use crate::{
   Component,
+  EventHandled,
   Node,
+  Path,
+  UiEvent,
 };
 
 impl Component for &str {
@@ -18,5 +21,28 @@ impl Component for String {
 impl Component for &String {
   fn render(&mut self) -> Node {
     Node::Text((*self).to_string())
+  }
+}
+
+impl<T> Component for Vec<T>
+where
+  T: Component,
+{
+  fn render(&mut self) -> Node {
+    let nodes = self.iter_mut().map(|i| i.render()).collect::<Vec<Node>>();
+    Node::Vec(nodes)
+  }
+
+  fn handle_ui_event(&mut self, event: &UiEvent, path: &Path) -> EventHandled {
+    // TODO maybe make this more functional
+    if let Some((first, rest)) = path.split_first() {
+      if let Some(target_node) = self.get_mut(*first) {
+        target_node.handle_ui_event(event, rest)
+      } else {
+        false
+      }
+    } else {
+      false
+    }
   }
 }
