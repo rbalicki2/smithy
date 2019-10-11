02365c40a6edba4266104c04c0b05ab786a1c749
Robert Balicki <robert.balicki@gmail.com>
2019-10-11 14:44:25 -0700

    stuff

--- crates/smithy_core/src/lib.rs
+++ crates/smithy_core/src/lib.rs
@@ -1,4 +1,4 @@
-use smithy_types::{
+pub use smithy_types::{
   AsInnerHtml,
   CollapsedNode,
   Component,
@@ -29,11 +29,13 @@ use self::node_diff::{
   Diffable,
 };
 
+pub use self::node_diff::DiffOperation;
+
 // TODO this should not be thread-local, but should be instantiated inside of
 // mount(). This will *probably* require us to not call crate::rerender in some
 // callbacks.
 thread_local! {
-  static ROOT_ELEMENT: RefCell<Option<Element>> = RefCell::new(None);
+  static ROOT_ELEMENT: RefCell<Option<Box<dyn RenderingTarget>>> = RefCell::new(None);
   static LAST_RENDERED_NODE: RefCell<Option<Vec<CollapsedNode>>> = RefCell::new(None);
   static ROOT_COMPONENT: RefCell<Option<Box<dyn Component>>> = RefCell::new(None);
   static EVENT_DEPTH: RefCell<u32> = RefCell::new(0);
@@ -43,9 +45,9 @@ fn get_window() -> Window {
   web_sys::window().unwrap()
 }
 
-fn render_initially(component: &mut Box<dyn Component>, el: &Element) {
+fn render_initially(component: &mut Box<dyn Component>, el: &Box<dyn RenderingTarget>) {
   let node: Vec<CollapsedNode> = component.render().into();
-  el.set_inner_html(&node.as_inner_html());
+  el.render(&node);
   LAST_RENDERED_NODE.store(node);
 }
 
@@ -78,47 +80,42 @@ fn handle_window_event(w: &WindowEvent) -> bool {
 }
 
 #[allow(dead_code)]
-fn handle_ui_event(ui_event: &UiEvent, path: &Path) -> bool {
+pub fn handle_ui_event(ui_event: &UiEvent, path: &Path) -> bool {
   with_increased_event_depth(|| {
     ROOT_COMPONENT
       .with_inner_value(|root_component| root_component.handle_ui_event(ui_event, &path))
   })
 }
 
-fn attach_listeners(el: &Element) {
-  let html_el = unsafe { transmute::<&Element, &js_fns::HTMLElement>(el) };
-  attach_event_listeners::attach_ui_event_listeners(&html_el);
-
-  let window = get_window();
-  let window = unsafe { transmute::<Window, js_fns::WINDOW>(window) };
-  attach_event_listeners::attach_window_event_listeners(&window);
-}
-
 /// Forces the currently mounted smithy app to re-render.
 pub fn rerender() {
   ROOT_COMPONENT.with_inner_value(|root_component| {
     let newly_rendered_nodes: Vec<CollapsedNode> = root_component.render().into();
-
-    LAST_RENDERED_NODE.with_inner_value(|last_rendered_node| {
-      let diff = last_rendered_node.get_diff_with(&newly_rendered_nodes);
-      #[cfg(feature = "browser-logs")]
-      web_sys::console::log_1(&wasm_bindgen::JsValue::from_str(&format!(
-        "\n\n\nrerender\n------------------------\n\nfrom {:?}\n\nto {:?}\n\ndiff {:#?}\n\n",
-        last_rendered_node.as_inner_html(),
-        newly_rendered_nodes.as_inner_html(),
-        diff
-      )));
-      ROOT_ELEMENT.with_inner_value(|el| {
-        #[cfg(feature = "browser-logs")]
-        web_sys::console::log_1(&wasm_bindgen::JsValue::from_str(&format!(
-          "\n\nroot el inner {:?}",
-          el.inner_html()
-        )));
-        for diff_item in diff.iter() {
-          diff_item.apply_to(el);
-        }
-      });
+    ROOT_ELEMENT.with_inner_value(|el| {
+      el.apply_diff(&newly_rendered_nodes);
     });
+
+    // LAST_RENDERED_NODE.with_inner_value(|last_rendered_node| {
+    //   let diff = last_rendered_node.get_diff_with(&newly_rendered_nodes);
+    //   #[cfg(feature = "browser-logs")]
+    //   web_sys::console::log_1(&wasm_bindgen::JsValue::from_str(&format!(
+    //     "\n\n\nrerender\n------------------------\n\nfrom {:?}\n\nto {:?}\n\ndiff {:#?}\n\n",
+    //     last_rendered_node.as_inner_html(),
+    //     newly_rendered_nodes.as_inner_html(),
+    //     diff
+    //   )));
+    //   ROOT_ELEMENT.with_inner_value(|el| {
+    //     #[cfg(feature = "browser-logs")]
+    //     web_sys::console::log_1(&wasm_bindgen::JsValue::from_str(&format!(
+    //       "\n\nroot el inner {:?}",
+    //       el.inner_html()
+    //     )));
+    //     for diff_item in diff.iter() {
+    //       el.apply_diff(diff_item);
+    //       // diff_item.apply_to(el);
+    //     }
+    //   });
+    // });
 
     root_component.handle_ref_assignment(vec![]);
     root_component.handle_post_render();
@@ -139,13 +136,37 @@ pub fn rerender() {
 ///   smithy::mount(app, el);
 /// }
 /// ```
-pub fn mount(mut component: Box<dyn Component>, el: Element) {
+pub fn mount(mut component: Box<dyn Component>, el: Box<dyn RenderingTarget>) {
   render_initially(&mut component, &el);
-  attach_listeners(&el);
+  el.attach_listeners();
   ROOT_ELEMENT.store(el);
   component.handle_ref_assignment(vec![]);
   component.handle_post_render();
   ROOT_COMPONENT.store(component);
+}
+
+pub trait RenderingTarget {
+  fn render(&self, nodes: &Vec<CollapsedNode>);
+  fn apply_diff(&self, nodes: &Vec<CollapsedNode>);
+  fn attach_listeners(&self);
+}
+
+impl RenderingTarget for Element {
+  fn render(&self, nodes: &Vec<CollapsedNode>) {
+    let inner_html = nodes.as_inner_html();
+    self.set_inner_html(&inner_html);
+  }
+
+  fn apply_diff(&self, nodes: &Vec<CollapsedNode>) {}
+
+  fn attach_listeners(&self) {
+    let html_el = unsafe { transmute::<&Element, &js_fns::HTMLElement>(self) };
+    attach_event_listeners::attach_ui_event_listeners(&html_el);
+
+    let window = get_window();
+    let window = unsafe { transmute::<Window, js_fns::WINDOW>(window) };
+    attach_event_listeners::attach_window_event_listeners(&window);
+  }
 }
 
 /// Converts a future into an `UnwrappedPromise`, which causes the
