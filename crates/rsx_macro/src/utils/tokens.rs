eb8ec15f835e7c17f14d37aaef8a241dc8aa4bb3
Robert Balicki <robert.balicki@gmail.com>
2019-10-13 15:23:40 -0700

    ready to try many_0_with_delimiter

--- crates/rsx_macro/src/utils/tokens.rs
+++ crates/rsx_macro/src/utils/tokens.rs
@@ -1,5 +1,9 @@
 use crate::prelude::*;
-use proc_macro2::Delimiter;
+use proc_macro2::{
+  Delimiter,
+  Punct,
+  Spacing,
+};
 
 pub fn match_group(input: TokenStream) -> TokenStreamIResult<TokenStream> {
   match_group_with_delimiter_opt(None)(input)
@@ -14,24 +18,64 @@ pub fn match_group_with_delimiter(
 fn match_group_with_delimiter_opt(
   delimiter: Option<Delimiter>,
 ) -> impl Fn(TokenStream) -> TokenStreamIResult<TokenStream> {
+  match_single_tree(move |t| match t {
+    TokenTree::Group(ref g) => {
+      if let Some(target_delimiter) = delimiter {
+        if g.delimiter() == target_delimiter {
+          Ok(((), g.stream()))
+        } else {
+          Err(Err::Error(((), ErrorKind::IsA)))
+        }
+      } else {
+        Ok(((), g.stream()))
+      }
+    },
+    _ => Err(Err::Error(((), ErrorKind::IsA))),
+  })
+}
+
+pub fn match_punct(
+  ch: Option<char>,
+  spacing: Option<Spacing>,
+) -> impl Fn(TokenStream) -> TokenStreamIResult<Punct> {
+  match_single_tree(move |t| match t {
+    TokenTree::Punct(ref p) => {
+      let mut failed_match = false;
+
+      if let Some(ch) = ch {
+        failed_match = failed_match || p.as_char() != ch;
+      }
+      if let Some(spacing) = spacing {
+        failed_match = failed_match || p.spacing() != spacing;
+      }
+
+      if !failed_match {
+        Ok(((), p.clone()))
+      } else {
+        Err(Err::Error(((), ErrorKind::IsA)))
+      }
+    },
+    _ => Err(Err::Error(((), ErrorKind::IsA))),
+  })
+}
+
+fn match_single_tree<T>(
+  f: impl Fn(&TokenTree) -> IResult<(), T>,
+) -> impl Fn(TokenStream) -> TokenStreamIResult<T> {
   move |input: TokenStream| {
     let vec = input.to_tree_vec();
     match vec.split_first() {
-      Some((first, rest)) => match first {
-        TokenTree::Group(ref g) => {
-          if let Some(target_delimiter) = delimiter {
-            if g.delimiter() == target_delimiter {
-              Ok((rest.to_token_stream(), g.stream()))
-            } else {
-              Err(Err::Error((input, ErrorKind::TakeTill1)))
-            }
-          } else {
-            Ok((rest.to_token_stream(), g.stream()))
-          }
-        },
-        _ => Err(Err::Error((input, ErrorKind::TakeTill1))),
+      Some((first, rest)) => {
+        let rest = rest.to_token_stream();
+        match f(first) {
+          Ok((_rest, matched)) => Ok((rest, matched)),
+          Err(e) => match e {
+            Err::Error((_rest, err)) => Err(Err::Error((rest, err))),
+            Err::Failure((_rest, err)) => Err(Err::Failure((rest, err))),
+            Err::Incomplete(e) => Err(Err::Incomplete(e)),
+          },
+        }
       },
-      // None => Err(Err::Incomplete(Needed::Size(1))),
       None => Err(Err::Error((input, ErrorKind::TakeTill1))),
     }
   }
