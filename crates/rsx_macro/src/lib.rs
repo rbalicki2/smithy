9ee02e97fddd186eb692ccee5d1872643938882e
Robert Balicki <robert.balicki@gmail.com>
2019-10-30 12:43:25 -0400

    better logging

--- crates/rsx_macro/src/lib.rs
+++ crates/rsx_macro/src/lib.rs
@@ -17,11 +17,12 @@ pub fn rsx_borrowed(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 fn rsx_inner(input: proc_macro::TokenStream, should_move: bool) -> proc_macro::TokenStream {
   let input: proc_macro2::TokenStream = input.into();
 
+  println!("\n\nparsing input: {}", input);
   let (rest, parsed) = parsers::parse_items(input).unwrap();
-  println!("final rest '{}'", rest);
+  println!("final rest (should be empty to not panic) '{}'", rest);
   let _ = crate::utils::ensure_consumed(rest).unwrap();
 
-  println!("final rsx parsed into\n{:?}", parsed);
+  println!("final rsx parsed into\n{:#?}", parsed);
   // input.into()
   quote::quote!(3).into()
 }
